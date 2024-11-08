8df52e1e4bead5abeb32fa3a5c677f671130e366
[Aung-Thu-Hein] <[aungthuhein225k@gmail.com]>
2024-11-08 12:47:41 +0700

    add auth, product home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ export default {
     extend: {
         colors: {
             adminPrimary: '#f59b42',
-        }
+            userPrimary: '#5280f2'
+        },
+        boxShadow: {
+            productCard: "0 8px 24px 0 rgba(16, 24, 40, 0.06)",
+          },
     },
   },
   plugins: [],
